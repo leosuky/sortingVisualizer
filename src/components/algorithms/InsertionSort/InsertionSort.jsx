767072dcb8565a92129ca3816dcafe9ec2989b43
Sunkanmi <leosuky99@gmail.com>
2022-05-05 15:27:28 +0100

    optimized colors for Insertion Sort

--- src/components/algorithms/InsertionSort/InsertionSort.jsx
+++ src/components/algorithms/InsertionSort/InsertionSort.jsx
@@ -1,4 +1,4 @@
-const BLUE = 0,
+const //BLUE = 0,
 	YELLOW = 1,
 	// PURPLE = 2,
 	RED = 3,
@@ -10,13 +10,12 @@ const InsertionSort = (array, arraySteps, colorSteps) => {
 	let colorKey = [...colorSteps[0]];
 
 	for (let i=0; i<size; i++) {
-		arraySteps.push(array.slice());
-		colorKey[i] = BLUE;
-		colorKey[0] = RED;
-		colorSteps.push(colorKey.slice());
 		
 		// if the element we're at is less than the first element
 		if (array[i] < array[0]) {
+			arraySteps.push(array.slice());
+			colorKey[0] = RED;
+			colorSteps.push(colorKey.slice());
 			// move the element to the first position
 			array.unshift(array.splice(i,1)[0]);			
 		} else {
@@ -24,20 +23,24 @@ const InsertionSort = (array, arraySteps, colorSteps) => {
 			for (let j=1; j<i; j++) {
 
 				arraySteps.push(array.slice());
-				colorKey[j] = BLUE;
-				colorKey[i] = YELLOW;
+				colorKey[j] = YELLOW;
 				colorSteps.push(colorKey.slice());
 
 				// if i > element before j but less than j at the same time...
 				if (array[i] >= array[j-1] && array[i] <= array[j]) {
+					arraySteps.push(array.slice());
+					colorKey[j] = RED;
+					colorSteps.push(colorKey.slice());
+
 					// insert the element i at index j (and then j's index becomes (j+1))
 					array.splice(j,0,array.splice(i,1)[0]);
 				}
+
+				arraySteps.push(array.slice());
+				colorKey[j] = GREEN;
+				colorSteps.push(colorKey.slice());
 			}
 
-			arraySteps.push(array.slice());
-			colorKey[i] = GREEN;
-			colorSteps.push(colorKey.slice());
 		}
 		arraySteps.push(array.slice());
 		colorKey[0] = GREEN;
